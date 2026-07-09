76de187b90a935ab388ac00538cb5a3fc7604474
Ankit Kumar <97714826+ankit-kumar-developer-122@users.noreply.github.com>
2026-07-09 19:23:10 +0530

    Binary Tree- Declare, Assign, Display in Binary Tree
    Binary Tree- Declare, Assign, Display

---
+++ Binary Tree- Declare, Assign, Display.java
@@ -0,0 +1,39 @@
+package BinaryTrees;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val = val;
+    }
+}
+public class Implementation {
+    public static void main(String[] args) {
+        //     3
+        //   4   2
+        // -1 1 6 9
+
+        Node a = new Node(3); // root
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+
+        a.left = b; a.right= c;
+        b.left = d; b.right= e;
+        c.left = f; c.right= g;
+        display(a);
+
+    }
+    private static void display(Node root){
+        if (root==null){ return ;}
+        System.out.print(root.val + " ");
+        display(root.left);
+        display(root.right);
+    }
+}
+
+// OUTPUT ->  3 4 -1 1 2 6 9 
